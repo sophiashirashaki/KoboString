10ce2ab40094cc636d67d959ef74c97893ed0590
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-14 17:12:02 +0800

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,6 +1,6 @@
 from pyrogram.types import InlineKeyboardButton
 
-
+class Data:
 generate_button = [
         [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
     ]
