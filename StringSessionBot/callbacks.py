dabeed4212453ee172e20125a8336c48b81d8414
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-14 16:59:45 +0800

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -1,5 +1,6 @@
 import traceback
 
+from Data import Data
 from pyrogram import Client
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from StringSessionBot.generate import generate_session
