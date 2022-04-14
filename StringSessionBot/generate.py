af3d4a2b283149388358b3e27f9d01917f8ee4b7
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-14 17:15:20 +0800

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -1,5 +1,4 @@
 from asyncio.exceptions import TimeoutError
-from Data import Data
 from pyrogram import Client, filters
 from telethon import TelegramClient
 from telethon.sessions import StringSession
